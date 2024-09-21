f30749d49f605caae94cedf09546a9fad3fa6ba7
jennyguidetti <j_guidetti95@hotmail.com>
2024-09-21 10:44:24 +1000

    Added pledges to views and model relations

--- crowdfunding/projects/models.py
+++ crowdfunding/projects/models.py
@@ -9,16 +9,26 @@ class Project(models.Model):
     is_open = models.BooleanField()
     date_created = models.DateTimeField(auto_now_add=True)
 
-class User(models.Model):
-    title = models.CharField(max_length=10)
-    first_name = models.CharField(max_length=20)
-    last_name = models.CharField(max_length=40)
-    age = models.IntegerField()
-    employer = models.TextField()
-    clinical_level = models.TextField()
-    years_experience = models.CharField(max_length=20)
-    email = models.TextField()
-    phone_number = models.CharField(max_length=10)
-    postcode = models.CharField(max_length=10)
-    is_open = models.BooleanField()
-    date_created = models.DateTimeField(auto_now_add=True)
+class Pledge(models.Model):
+    amount = models.IntegerField()
+    comment = models.CharField(max_length=200)
+    anonymous = models.BooleanField()
+    project = models.ForeignKey(
+        'Project',
+        on_delete=models.CASCADE,
+        related_name='pledges'
+    )
+
+# class User(models.Model):
+#     title = models.CharField(max_length=10)
+#     first_name = models.CharField(max_length=20)
+#     last_name = models.CharField(max_length=40)
+#     age = models.IntegerField()
+#     employer = models.TextField()
+#     clinical_level = models.TextField()
+#     years_experience = models.CharField(max_length=20)
+#     email = models.TextField()
+#     phone_number = models.CharField(max_length=10)
+#     postcode = models.CharField(max_length=10)
+#     is_open = models.BooleanField()
+#     date_created = models.DateTimeField(auto_now_add=True)
--- crowdfunding/projects/views.py
+++ crowdfunding/projects/views.py
@@ -2,8 +2,27 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from django.http import Http404
-from .models import Project
-from .serializers import ProjectSerializer
+from .models import Project, Pledge
+from .serializers import ProjectSerializer, PledgeSerializer, ProjectDetailSerializer
+
+class PledgeList(APIView):
+    def get(self, request):
+        pledges = Pledge.objects.all()
+        serializer = PledgeSerializer(pledges, many=True)
+        return Response(serializer.data)
+
+    def post(self, request):
+        serializer = PledgeSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(
+                serializer.data,
+                status=status.HTTP_201_CREATED
+            )
+        return Response(
+            serializer.errors,
+            status=status.HTTP_400_BAD_REQUEST
+        )
 
 class ProjectList(APIView):
     def get(self, request):
@@ -34,5 +53,5 @@ class ProjectDetail(APIView):
 
     def get(self, request, pk):
         project = self.get_object(pk)
-        serializer = ProjectSerializer(project)
+        serializer = ProjectDetailSerializer(project)
         return Response(serializer.data)
