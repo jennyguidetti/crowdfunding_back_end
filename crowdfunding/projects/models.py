61d999d8c9c6078a05a10981959ab139655604ab
jennyguidetti <j_guidetti95@hotmail.com>
2024-09-28 10:24:16 +1000

    authentication tokens added

--- crowdfunding/projects/models.py
+++ crowdfunding/projects/models.py
@@ -1,6 +1,6 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
-# Create your models here.
 class Project(models.Model):
     title = models.CharField(max_length=200)
     description = models.TextField()
@@ -8,6 +8,11 @@ class Project(models.Model):
     image = models.URLField()
     is_open = models.BooleanField()
     date_created = models.DateTimeField(auto_now_add=True)
+    owner = models.ForeignKey(
+        get_user_model(),
+        on_delete=models.CASCADE,
+        related_name='owned_projects'
+    )
 
 class Pledge(models.Model):
     amount = models.IntegerField()
@@ -15,6 +20,11 @@ class Pledge(models.Model):
     anonymous = models.BooleanField()
     project = models.ForeignKey(
         'Project',
+        on_delete=models.CASCADE,
+        related_name='pledges'
+    )
+    supporter = models.ForeignKey(
+        get_user_model(),
         on_delete=models.CASCADE,
         related_name='pledges'
     )
--- crowdfunding/projects/views.py
+++ crowdfunding/projects/views.py
@@ -14,7 +14,7 @@ class PledgeList(APIView):
     def post(self, request):
         serializer = PledgeSerializer(data=request.data)
         if serializer.is_valid():
-            serializer.save()
+            serializer.save(owner=request.user)
             return Response(
                 serializer.data,
                 status=status.HTTP_201_CREATED
@@ -33,7 +33,7 @@ class ProjectList(APIView):
     def post(self, request):
         serializer = ProjectSerializer(data=request.data)
         if serializer.is_valid():
-            serializer.save()
+            serializer.save(owner=request.user)
             return Response(
                 serializer.data,
                 status=status.HTTP_201_CREATED
