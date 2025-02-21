663d8f1d5df526c7f3981e64df92910bcfcc40b9
jennyguidetti <j_guidetti95@hotmail.com>
2025-02-21 16:53:11 +1030

    added unique requirement to users emails, updated gitignore to correct files

--- crowdfunding/users/models.py
+++ crowdfunding/users/models.py
@@ -2,6 +2,7 @@ from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 class CustomUser(AbstractUser):
+    email = models.EmailField(unique=True)
     employer = models.CharField(max_length=50)
     clinical_level = models.CharField(max_length=50)
 
